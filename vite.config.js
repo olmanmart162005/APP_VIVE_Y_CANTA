0779e8eb089df39c816a9e752389f06dba3a58ca
olmanmart162005 <olme_martinezm56@unicah.edu>
2026-05-28 10:49:23 -0600

    feat: iconos PWA

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
   plugins: [
-
     react(),
-
     VitePWA({
-
-      registerType:
-        "autoUpdate",
+      registerType: "autoUpdate",
+      injectRegister: "auto",
 
       devOptions: {
         enabled: true,
@@ -18,78 +15,60 @@ export default defineConfig({
 
       includeAssets: [
         "logo.png",
+        "icon_192.png",
+        "icon_512.png",
       ],
 
       manifest: {
-
         id: "/",
-
-        name:
-          "Coro Vive y Canta",
-
-        short_name:
-          "Vive y Canta",
-
-        description:
-          "Sistema Administrativo del Coro Vive y Canta",
-
-        theme_color:
-          "#D4AF37",
-
-        background_color:
-          "#F8F4E9",
-
-        display:
-          "standalone",
-
-        orientation:
-          "portrait",
-
-        scope:
-          "/",
-
-        start_url:
-          "/",
-
-        lang:
-          "es",
-
+        name: "Coro Vive y Canta",
+        short_name: "Vive y Canta",
+        description: "Sistema Administrativo del Coro Vive y Canta",
+        theme_color: "#D4AF37",
+        background_color: "#F8F4E9",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
+        lang: "es",
         icons: [
           {
-            src:
-              "/logo.png",
-            sizes:
-              "192x192",
-            type:
-              "image/png",
-            purpose:
-              "any maskable",
+            src: "/icon_192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
           },
-
           {
-            src:
-              "/logo.png",
-            sizes:
-              "512x512",
-            type:
-              "image/png",
-            purpose:
-              "any maskable",
+            src: "/icon_192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icon_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
 
       workbox: {
-        globPatterns: [
-          "**/*.{js,css,html,png,svg,ico}"
-        ]
-      }
-
+        globPatterns: ["**/*.{js,css,html,png,svg,ico,woff2}"],
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+      },
     }),
   ],
 
   build: {
-    chunkSizeWarningLimit:
-      1600,
+    chunkSizeWarningLimit: 1600,
   },
 })
